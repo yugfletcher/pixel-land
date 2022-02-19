99393ae773a298e0086e921144244a64cd517697
fletcher <jackfletcher68@gmail.com>
2022-02-19 14:56:41 -0500

    add rotating cube example

--- src/script.js
+++ src/script.js
@@ -1,6 +1,72 @@
-import './style.css'; import * as THREE from 'three'; import 
-{OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js'; /**
+import './style.css';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+
+/**
  * Base
- */ // Canvas const canvas = document.querySelector('canvas.webgl'); // 
-Scene
+ */
+// Canvas
+const canvas = document.querySelector('canvas.webgl');
+
+// Sizes
+const sizes = {
+    width: 800,
+    height: 600,
+}
+
+// Scene
 const scene = new THREE.Scene();
+
+// Object
+const mesh = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1, 5, 5, 5),
+    new THREE.MeshBasicMaterial({color: 0xff0000})
+);
+scene.add(mesh);
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 1, 1000);
+camera.position.x = 2
+camera.position.y = 2
+camera.position.z = 2;
+camera.lookAt(mesh.position);
+scene.add(camera);
+
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+});
+renderer.setSize(sizes.width, sizes.height);
+
+// Cursor
+const cursor = {
+    x: 0,
+    y: 0,
+};
+
+window.addEventListener('mousemove', (event) => {
+    cursor.x = event.clientX / sizes.width - 0.5;
+    cursor.y = -(event.clientY / sizes.height - 0.5);
+
+    console.log(cursor.x, cursor.y);
+});
+
+// Animate
+const clock = new THREE.Clock();
+
+const tick = () => {
+    // Update objects
+    mesh.rotation.y = clock.getElapsedTime();
+
+    // Render
+    controls.update();
+    renderer.render(scene, camera);
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+};
+
+tick();
